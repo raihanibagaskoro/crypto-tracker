1e29ac6d8cbbcf20f086fc2b9d6449113f1dea4f
Raihan Ihsan Bagaskoro <raihanibagaskoro@gmail.com>
2024-12-09 14:20:43 +0700

    init: websocket

--- src/main/java/com/example/crypto_tracker/CryptoTrackerApplication.java
+++ src/main/java/com/example/crypto_tracker/CryptoTrackerApplication.java
@@ -2,10 +2,13 @@ package com.example.crypto_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableWebSocketMessageBroker
 public class CryptoTrackerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoTrackerApplication.class, args);
 	}
---
+++ src/main/java/com/example/crypto_tracker/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.example.crypto_tracker.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${websocket.endpoint}")
+    private String endpoint;
+
+    @Value("${websocket.topic.crypto}")
+    private String cryptoTopic;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint(endpoint)
+                .setAllowedOrigins("http://localhost:5173")
+                .withSockJS();
+    }
+}
